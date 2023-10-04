458e2a83d39ab2aae42680d4465ac98dc28731f5
Hacker3331 <139943514+Hacker3331@users.noreply.github.com>
2023-10-04 20:33:51 +0700

    KorbloxTest.lua

---
+++ KorbloxTest.lua
@@ -0,0 +1,10 @@
+game.Players.PlayerAdded:Connect(function(player) 
+    player.CharacterAdded:Connect(function(chr) 
+      
+      local Humanoid = chr:WaitForChild("Humanoid")
+      local Desc = Humanoid:GetAppliedDescription()
+      Desc.RightLeg = 139607718
+
+     end)
+    end)
+end)
